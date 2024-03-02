e1b33caab8dcb7e627043d5250c7202a13da72bb
KevKibe <kchegz234@gmail.com>
2024-03-02 13:58:42 +0300

    update

--- load_data.py
+++ load_data.py
@@ -2,11 +2,10 @@ import os
 from dotenv import load_dotenv  
 from datasets import load_dataset, DatasetDict
 
-# load_dotenv()
 
 class LoadData():
     def __init__(self):
-        self.huggingface_token = os.getenv("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") 
+        self.huggingface_token = "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"
         self.language_abbr = "sw"
         self.dataset_name = "mozilla-foundation/common_voice_13_0"
 
@@ -22,41 +21,7 @@ class LoadData():
 
 
 
-# data = LoadData()
-# dataset = data.download_dataset()
-# print(dataset)
 
-
-
-
-# load_dotenv()
-
-# def download_dataset(huggingface_token, language_abbr, dataset_name):
-#     if not huggingface_token:
-#         raise ValueError("Hugging Face token not found. Please check your .env file.")
-
-#     try:
-#         common_voice = DatasetDict()
-#         # Assuming you've confirmed that the dataset exists and is accessible
-#         # Uncomment the following line if you need to download the training set as well
-#         # common_voice["train"] = load_dataset(dataset_name, language_abbr, split="train[:10]", use_auth_token=huggingface_token, streaming=True)
-#         common_voice["test"] = load_dataset(dataset_name, language_abbr, split="test", use_auth_token=huggingface_token, streaming=True)
-#         return common_voice
-#     except Exception as e:
-#         print(f"An error occurred while downloading the dataset: {e}")
-#         return None
-
-# # Define your parameters
-# HF_TOKEN = os.getenv("HF_TOKEN")
-# LANGUAGE_ABBR = "sw"
-# DATASET_NAME = "mozilla-foundation/common_voice_13_0"
-
-# # Download the dataset
-# dataset = download_dataset(HF_TOKEN, LANGUAGE_ABBR, DATASET_NAME)
-# if dataset:
-#     print("Dataset downloaded successfully.")
-# else:
-#     print("Failed to download the dataset.")
 
 
 
