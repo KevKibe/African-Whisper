41d6ac727f868ad131226ac97998ba76071bbc7a
KevKibe <kchegz234@gmail.com>
2024-03-04 08:25:58 +0300

    update

--- main.py
+++ main.py
@@ -12,12 +12,15 @@ dataset = preprocessor.remove_columns()
 # prepared_test_dataset = dataset["test"].map(preprocessor.prepare_dataset)
 tokenizer = preprocessor.tokenizer()
 pprint(type(dataset["train"]))
-input_str = dataset["train"][0]["sentence"]
-labels = tokenizer(input_str).input_ids
-decoded_with_special = tokenizer.decode(labels, skip_special_tokens=False)
-decoded_str = tokenizer.decode(labels, skip_special_tokens=True)
-print(f"Input:                 {input_str}")
-print(f"Decoded w/ special:    {decoded_with_special}")
-print(f"Decoded w/out special: {decoded_str}")
-print(f"Are equal:             {input_str == decoded_str}")
+for i, example in enumerate(dataset["train"]):
+    if i == 0:  # If you just want to process the first item for demonstration
+        input_str = example["sentence"]
+        labels = tokenizer(input_str).input_ids
+        decoded_with_special = tokenizer.decode(labels, skip_special_tokens=False)
+        decoded_str = tokenizer.decode(labels, skip_special_tokens=True)
+        print(f"Input:                 {input_str}")
+        print(f"Decoded w/ special:    {decoded_with_special}")
+        print(f"Decoded w/out special: {decoded_str}")
+        print(f"Are equal:             {input_str == decoded_str}")
+    break  
 # print(prepared_test_dataset)
