e9bb300a28165f1b36094565d13eaa4908eaab2a
KevKibe <kchegz234@gmail.com>
2024-03-05 17:06:02 +0300

    addition of huggingface push token argument

--- src/main.py
+++ src/main.py
@@ -3,7 +3,8 @@ from trainer import Trainer
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Run the training orchestrator with specified parameters.")
-    parser.add_argument("--huggingface_token", type=str, required=True, help="Hugging Face API token for authenticated access.")
+    parser.add_argument("--huggingface_read_token", type=str, required=True, help="Hugging Face API token for read authenticated access.")
+    parser.add_argument("--huggingface_push_token", type=str, required=True, help="Hugging Face API token for write authenticated access.")
     parser.add_argument("--dataset_name", type=str, default="mozilla-foundation/common_voice_16_1", help="Name of the dataset to be downloaded from Hugging Face.")
     parser.add_argument("--language_abbr", type=str, default="sw", help="Abbreviation of the language for the dataset.")
     parser.add_argument("--model_id", type=str, default="openai/whisper-small", help="Model ID for the model to be used in training.")
@@ -15,7 +16,8 @@ if __name__ == "__main__":
     args = parse_args()
     
     orchestrator = Trainer(
-        huggingface_token=args.huggingface_token,
+        huggingface_read_token=args.huggingface_read_token,
+        huggingface_push_token=args.huggingface_push_token,
         dataset_name=args.dataset_name,
         language_abbr=args.language_abbr,
         model_id=args.model_id,
