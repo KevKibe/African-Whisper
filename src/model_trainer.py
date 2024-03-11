92acac4989e26542f86e87534ff129fdcf96a125
KevKibe <kchegz234@gmail.com>
2024-03-11 18:13:15 +0300

    update: better seq2seq training arguments

--- src/model_trainer.py
+++ src/model_trainer.py
@@ -1,5 +1,5 @@
 import os
-from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer
+from transformers import Seq2SeqTrainingArguments, Seq2SeqTrainer, PushToHubCallback
 from collator import DataCollatorSpeechSeq2SeqWithPadding
 import evaluate
 from datasets import DatasetDict
@@ -7,7 +7,6 @@ from wandb_callback import WandbProgressResultsCallback
 from transformers.trainer_pt_utils import IterableDatasetShard
 from torch.utils.data import IterableDataset
 from transformers import TrainerCallback
-from audio_data_processor import AudioDataProcessor
 from whisper_model_prep import WhisperModelPrep
 
 class ShuffleCallback(TrainerCallback):
@@ -61,12 +60,12 @@ class Trainer:
         pred_ids = pred.predictions
         label_ids = pred.label_ids
         label_ids[label_ids == -100] = self.tokenizer.pad_token_id
-        pred_str = self.tokenizer.batch_decode(pred_ids, skip_special_tokens=True)
-        label_str = self.tokenizer.batch_decode(label_ids, skip_special_tokens=True)
+        pred_str = self.tokenizer.batch_decode(pred_ids, skip_special_tokens=True, normalize = True)
+        label_str = self.tokenizer.batch_decode(label_ids, skip_special_tokens=True, normalize = True)
         wer = 100 * self.metric.compute(predictions=pred_str, references=label_str)
         return {"wer": wer}
     
-    def compute_spectrograms(self, example):
+    def compute_spectrograms(self, example) ->  dict:
         waveform =  example["audio"]["array"]
         model_prep = WhisperModelPrep(self.dataset, self.model_id, self.language_abbr, 'transcribe')
         feature_extractor = model_prep.initialize_feature_extractor()
@@ -82,42 +81,51 @@ class Trainer:
         data_collator = DataCollatorSpeechSeq2SeqWithPadding(processor=self.feature_processor)
         training_args = Seq2SeqTrainingArguments(
             output_dir=f"./{self.model_id}-{self.language_abbr}",  
-            per_device_train_batch_size=16,
+            per_device_train_batch_size=64,
             gradient_accumulation_steps=1,
             learning_rate=1e-5,
-            warmup_steps=50,
-            max_steps=50,
+            warmup_steps=100,
+            max_steps=200,
             gradient_checkpointing=True,
             fp16=False,
+            optim="adamw_bnb_8bit",
             evaluation_strategy="steps",
-            per_device_eval_batch_size=8,
+            per_device_eval_batch_size=32,
             predict_with_generate=True,
             generation_max_length=225,
-            save_steps=25,
-            eval_steps=25,
-            logging_steps=10,
+            save_steps=50,
+            eval_steps=50,
+            logging_steps=100,
             load_best_model_at_end=True,
             metric_for_best_model="wer",
             greater_is_better=False,
             push_to_hub = True,
+            hub_strategy="checkpoint",
             hub_token = self.huggingface_write_token,
-            report_to = "wandb"
-
+            report_to = "wandb",
+            remove_unused_columns=False, 
+            ignore_data_skip=True
         )
+
+        eval_dataset = self.dataset["test"].map(self.compute_spectrograms)
 
         trainer = Seq2SeqTrainer(
             args=training_args,
             model=self.model,
             train_dataset=self.dataset["train"],
-            eval_dataset=self.dataset["test"],
+            eval_dataset=eval_dataset,
             data_collator=data_collator,
             compute_metrics=self.compute_metrics,
             tokenizer=self.feature_processor.feature_extractor,
             callbacks=[ShuffleCallback()],            
-        )        
-        samples_dataset = self.dataset["test"].map(self.compute_spectrograms)
+        )   
+
         model_prep = WhisperModelPrep(self.dataset, self.model_id, self.language_abbr, 'transcribe')
         tokenizer = model_prep.initialize_tokenizer()
-        progress_callback = WandbProgressResultsCallback(trainer, samples_dataset, tokenizer)
+        tokenizer.save_pretrained(training_args.output_dir)
+
+        trainer.add_callback(PushToHubCallback(output_dir=training_args.output_dir, tokenizer=tokenizer))
+
+        progress_callback = WandbProgressResultsCallback(trainer, eval_dataset, tokenizer)
         trainer.add_callback(progress_callback)
         trainer.train()
