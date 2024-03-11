3b75f81a4b32631dad37102fa7d1eaec3b8ac8b8
KevKibe <kchegz234@gmail.com>
2024-03-11 09:10:42 +0300

    fix: push arguement to write

--- src/main.py
+++ src/main.py
@@ -5,13 +5,13 @@ from model_trainer import Trainer
 def parse_args():
     parser = argparse.ArgumentParser(description="Run the training orchestrator with specified parameters.")
     parser.add_argument("--huggingface_read_token", type=str, required=True, help="Hugging Face API token for read authenticated access.")
-    parser.add_argument("--huggingface_push_token", type=str, required=True, help="Hugging Face API token for write authenticated access.")
+    parser.add_argument("--huggingface_write_token", type=str, required=True, help="Hugging Face API token for write authenticated access.")
     parser.add_argument("--dataset_name", type=str, default="mozilla-foundation/common_voice_16_1", help="Name of the dataset to be downloaded from Hugging Face.")
     parser.add_argument("--language_abbr", type=str, default="sw", help="Abbreviation of the language for the dataset.")
     parser.add_argument("--model_id", type=str, default="openai/whisper-small", help="Model ID for the model to be used in training.")
     parser.add_argument("--processing_task", type=str, default="transcribe", help="The processing task to be performed.")
     parser.add_argument("--wandb_api_key", type=str, help="The wandb.ai api key for monitoring training runs, signup and generate an api key.")
-    
+
     return parser.parse_args()
 
 if __name__ == "__main__":
@@ -25,9 +25,8 @@ if __name__ == "__main__":
     )
     tokenizer, feature_extractor, feature_processor, model = process.prepare_model()
     processed_dataset = process.load_dataset(feature_extractor, tokenizer)
-
-    orchestrator = Trainer(
-            huggingface_push_token=args.huggingface_push_token,
+    trainer = Trainer(
+            huggingface_write_token=args.huggingface_write_token,
             model_id=args.model_id,
             dataset=processed_dataset,
             model=model,
@@ -37,6 +36,7 @@ if __name__ == "__main__":
             language_abbr=args.language_abbr,
             wandb_api_key=args.wandb_api_key
             )
-    orchestrator.train()
+    trainer.train()
+    
     
     
