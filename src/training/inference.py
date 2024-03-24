0ddc3748ac2e4ec24754d71f44308f3faf5b38dd
KevKibe <kchegz234@gmail.com>
2024-03-24 18:03:29 +0300

    update: functional setup

--- src/training/inference.py
+++ src/training/inference.py
@@ -2,36 +2,34 @@ import torch
 import gradio as gr
 import pytube as pt
 from transformers import pipeline, WhisperTokenizer
-from huggingface_hub import model_info
 import argparse
-
 
 def parse_args():
     parser = argparse.ArgumentParser(description="Whisper Demo: Transcribe Audio and YouTube")
     parser.add_argument("--model_name", type=str, help="Name of the fine-tuned model to use in your huggingfacehub repo")
     parser.add_argument("--language_abbr", type=str, help="Language abbreviation for transcription")
-    parser.add_argument("--tokenizer", type = str, help = "Whisper model version you used to fine-tune your model e.g openai/whisper-tiny, openai/whisper-base, openai/whisper-small, openai/whisper-medium, openai/whisper-large, openai/whisper-large-v2")
-    parser.add_argument("--huggingface_read_token", type = str, help = "Hugging Face API token for read authenticated access.")
+    parser.add_argument("--tokenizer", type=str, help="Whisper model version you used to fine-tune your model e.g openai/whisper-tiny, openai/whisper-base, openai/whisper-small, openai/whisper-medium, openai/whisper-large, openai/whisper-large-v2")
+    parser.add_argument("--huggingface_read_token", type=str, help="Hugging Face API token for read authenticated access.")
     return parser.parse_args()
 
-args = parse_args()
+def initialize_pipeline(model_name, language_abbr, tokenizer, huggingface_read_token):
+    tokenizer = WhisperTokenizer.from_pretrained(tokenizer)
 
+    device = 0 if torch.cuda.is_available() else "cpu"
+    pipe = pipeline(
+        task="automatic-speech-recognition",
+        model=model_name,
+        token=huggingface_read_token,
+        tokenizer=tokenizer,
+        chunk_length_s=30,
+        device=device,
+    )
 
-tokenizer = WhisperTokenizer.from_pretrained(args.tokenizer)
+    pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(language=language_abbr, task="transcribe")
 
-device = 0 if torch.cuda.is_available() else "cpu"
-pipe = pipeline(
-    task="automatic-speech-recognition",
-    model=args.model_name,
-    token = args.huggingface_read_token,
-    tokenizer=tokenizer, 
-    chunk_length_s=30,
-    device=device,
-)
-print(pipe.model.config.model_type)
-pipe.model.config.forced_decoder_ids = pipe.tokenizer.get_decoder_prompt_ids(language=args.language_abbr, task="transcribe")
+    return pipe
 
-def transcribe(microphone, file_upload):
+def transcribe(microphone, file_upload, pipe):
     warn_output = ""
     if (microphone is not None) and (file_upload is not None):
         warn_output = (
@@ -48,19 +46,13 @@ def transcribe(microphone, file_upload):
 
     return warn_output + text
 
-
-def _return_yt_html_embed(yt_url):
+def yt_transcribe(yt_url, pipe):
+    yt = pt.YouTube(yt_url)
     video_id = yt_url.split("?v=")[-1]
-    HTML_str = (
+    html_embed_str = (
         f'<center> <iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe>'
         " </center>"
     )
-    return HTML_str
-
-
-def yt_transcribe(yt_url):
-    yt = pt.YouTube(yt_url)
-    html_embed_str = _return_yt_html_embed(yt_url)
     stream = yt.streams.filter(only_audio=True)[0]
     stream.download(filename="audio.mp3")
 
@@ -68,38 +60,41 @@ def yt_transcribe(yt_url):
 
     return html_embed_str, text
 
-demo = gr.Blocks()
+def generate_demo(model_name, language_abbr, tokenizer, huggingface_read_token):
+    pipe = initialize_pipeline(model_name, language_abbr, tokenizer, huggingface_read_token)
 
-mf_transcribe = gr.Interface(
-    fn=transcribe,
-    inputs=[
-        gr.Audio(sources="microphone", type="filepath"),
-        gr.Audio(sources="upload", type="filepath"),
-    ],
-    outputs="text",
-    title="Whisper Demo: Transcribe Audio",
-    description=(
-        "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the the fine-tuned"
-        f" checkpoint [{args.model_name}](https://huggingface.co/{args.model_name}) and 🤗 Transformers to transcribe audio files"
-        " of arbitrary length."
-    ),
-    allow_flagging="never",
-)
+    mf_transcribe = gr.Interface(
+        fn=transcribe,
+        inputs=[
+            gr.Audio(sources="microphone", type="filepath"),
+            gr.Audio(sources="upload", type="filepath"),
+        ],
+        outputs="text",
+        title="Whisper Demo: Transcribe Audio",
+        description=(
+            "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the fine-tuned"
+            f" checkpoint [{model_name}](https://huggingface.co/{model_name}) and 🤗 Transformers to transcribe audio files"
+            " of arbitrary length."
+        ),
+        allow_flagging="never",
+    )
 
-yt_transcribe = gr.Interface(
-    fn=yt_transcribe,
-    inputs=[gr.Textbox(lines=1, placeholder="Paste the URL to a YouTube video here", label="YouTube URL")],
-    outputs=["html", "text"],
-    title="Whisper Demo: Transcribe YouTube",
-    description=(
-        "Transcribe long-form YouTube videos with the click of a button! Demo uses the the fine-tuned checkpoint:"
-        f" [{args.model_name}](https://huggingface.co/{args.model_name}) and 🤗 Transformers to transcribe audio files of"
-        " arbitrary length."
-    ),
-    allow_flagging="never",
-)
+    yt_transcribe_interface = gr.Interface(
+        fn=yt_transcribe,
+        inputs=[gr.Textbox(lines=1, placeholder="Paste the URL to a YouTube video here", label="YouTube URL")],
+        outputs=["html", "text"],
+        title="Whisper Demo: Transcribe YouTube",
+        description=(
+            "Transcribe long-form YouTube videos with the click of a button! Demo uses the fine-tuned checkpoint:"
+            f" [{model_name}](https://huggingface.co/{model_name}) and 🤗 Transformers to transcribe audio files of"
+            " arbitrary length."
+        ),
+        allow_flagging="never",
+    )
 
-with demo:
-    gr.TabbedInterface([mf_transcribe, yt_transcribe], ["Transcribe Audio", "Transcribe YouTube"])
+    demo = gr.TabbedInterface([mf_transcribe, yt_transcribe_interface], ["Transcribe Audio", "Transcribe YouTube"])
+    demo.launch(share=True)
 
-demo.launch(share = True)
+if __name__ == "__main__":
+    args = parse_args()
+    generate_demo(args.model_name, args.language_abbr, args.tokenizer, args.huggingface_read_token)
