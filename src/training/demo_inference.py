7f5881d3c81a5866ce64a4fe543dafcb4d204f89
KevKibe <kchegz234@gmail.com>
2024-03-28 16:10:44 +0300

    update: gradio inference

--- src/training/demo_inference.py
+++ src/training/demo_inference.py
@@ -2,94 +2,58 @@ import torch
 import gradio as gr
 import pytube as pt
 from transformers import pipeline, WhisperTokenizer
-
+import os
 
 class WhisperDemo:
-    def __init__(self, model_name, language_abbr, tokenizer, huggingface_read_token):
+    def __init__(self, model_name, huggingface_read_token):
         self.model_name = model_name
-        self.language_abbr = language_abbr
-        self.tokenizer = tokenizer
         self.huggingface_read_token = huggingface_read_token
         self.pipe = None
 
     def initialize_pipeline(self):
-        tokenizer = WhisperTokenizer.from_pretrained(self.tokenizer)
         device = 0 if torch.cuda.is_available() else "cpu"
+        os.environ["HF_TOKEN"] = self.huggingface_read_token
         self.pipe = pipeline(
             task="automatic-speech-recognition",
             model=self.model_name,
             token=self.huggingface_read_token,
-            tokenizer=tokenizer,
             chunk_length_s=30,
             device=device,
         )
-        self.pipe.model.config.forced_decoder_ids = (
-            self.pipe.tokenizer.get_decoder_prompt_ids(
-                language=self.language_abbr, task="transcribe"
-            )
-        )
 
-    def transcribe(self, microphone, file_upload):
-        warn_output = ""
-        if (microphone is not None) and (file_upload is not None):
-            warn_output = (
-                "WARNING: You've uploaded an audio file and used the microphone. "
-                "The recorded file from the microphone will be used and the uploaded audio will be discarded.\n"
-            )
-
-        elif (microphone is None) and (file_upload is None):
-            return (
-                "ERROR: You have to either use the microphone or upload an audio file"
-            )
-
-        file = microphone if microphone is not None else file_upload
-        text = self.pipe(file)["text"]
-
-        return warn_output + text
-
-    def yt_transcribe(self, yt_url):
-        yt = pt.YouTube(yt_url)
-        video_id = yt_url.split("?v=")[-1]
-        html_embed_str = (
-            f'<center> <iframe width="500" height="320" src="https://www.youtube.com/embed/{video_id}"> </iframe>'
-            " </center>"
-        )
-        stream = yt.streams.filter(only_audio=True)[0]
-        stream.download(filename="audio.mp3")
-        text = self.pipe("audio.mp3")["text"]
-
-        return html_embed_str, text
+    def transcribe(self, inputs, task):
+        if input is None:
+            raise gr.Error("No audio file submitted! Please upload or record an audio file before submitting your request.")
+        BATCH_SIZE = 8
+        text = self.pipe(inputs, batch_size=BATCH_SIZE, generate_kwargs={"task": task}, return_timestamps=True)["text"]
+        return  text
 
     def generate_demo(self):
         self.initialize_pipeline()
-
         mf_transcribe = gr.Interface(
             fn=self.transcribe,
             inputs=[
                 gr.Audio(sources="microphone", type="filepath"),
-                gr.Audio(sources="upload", type="filepath"),
-                gr.inputs.Radio(["transcribe", "translate"], label="Task", default="transcribe")
+                gr.Radio(["transcribe", "translate"], label="Task"),
             ],
             outputs="text",
-            title="Whisper Demo: Transcribe Audio",
+            title="Whisper : Transcribe Audio",
             description=(
-                "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the fine-tuned"
+                "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the OpenAI Whisper"
                 f" checkpoint [{self.model_name}](https://huggingface.co/{self.model_name}) and 🤗 Transformers to transcribe audio files"
                 " of arbitrary length."
             ),
             allow_flagging="never",
-        )
+            )
 
         file_transcribe = gr.Interface(
             fn=self.transcribe,
             inputs=[
-                gr.inputs.Audio(source="upload", type="filepath", optional=True, label="Audio file"),
-                gr.inputs.Radio(["transcribe", "translate"], label="Task", default="transcribe")
+                gr.Audio(sources="upload", type="filepath", label="Audio file"),
+                gr.Radio(["transcribe", "translate"], label="Task"),
             ],
             outputs="text",
-            layout="horizontal",
-            theme="huggingface",
-            title="Whisper Large V3: Transcribe Audio",
+            title="Whisper : Transcribe Audio",
             description=(
                 "Transcribe long-form microphone or audio inputs with the click of a button! Demo uses the OpenAI Whisper"
                 f" checkpoint [{self.model_name}](https://huggingface.co/{self.model_name}) and 🤗 Transformers to transcribe audio files"
@@ -98,28 +62,8 @@ class WhisperDemo:
             allow_flagging="never",
         )
 
-        yt_transcribe_interface = gr.Interface(
-            fn=self.yt_transcribe,
-            inputs=[
-                gr.Textbox(
-                    lines=1,
-                    placeholder="Paste the URL to a YouTube video here",
-                    label="YouTube URL",
-                ),
-                gr.inputs.Radio(["transcribe", "translate"], label="Task", default="transcribe")
-            ],
-            outputs=["html", "text"],
-            title="Whisper Demo: Transcribe YouTube",
-            description=(
-                "Transcribe long-form YouTube videos with the click of a button! Demo uses the fine-tuned checkpoint:"
-                f" [{self.model_name}](https://huggingface.co/{self.model_name}) and 🤗 Transformers to transcribe audio files of"
-                " arbitrary length."
-            ),
-            allow_flagging="never",
-        )
-
         demo = gr.TabbedInterface(
-            [mf_transcribe, yt_transcribe_interface],
+            [mf_transcribe, file_transcribe],
             ["Transcribe Audio", "Transcribe YouTube"],
         )
         demo.launch(share=True)
--- src/training/gradio_demo.py
+++ src/training/gradio_demo.py
@@ -11,14 +11,7 @@ def parse_args():
         type=str,
         help="Name of the fine-tuned model to use in your huggingfacehub repo",
     )
-    parser.add_argument(
-        "--language_abbr", type=str, help="Language abbreviation for transcription"
-    )
-    parser.add_argument(
-        "--tokenizer",
-        type=str,
-        help="Whisper model version you used to fine-tune your model e.g openai/whisper-tiny, openai/whisper-base, openai/whisper-small, openai/whisper-medium, openai/whisper-large, openai/whisper-large-v2",
-    )
+
     parser.add_argument(
         "--huggingface_read_token",
         type=str,
@@ -30,6 +23,6 @@ def parse_args():
 if __name__ == "__main__":
     args = parse_args()
     demo = WhisperDemo(
-        args.model_name, args.language_abbr, args.tokenizer, args.huggingface_read_token
+        args.model_name, args.huggingface_read_token
     )
     demo.generate_demo()
