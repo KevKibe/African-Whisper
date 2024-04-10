b3c05022686409e076928b3920523317ea8d94ef
KevKibe <kchegz234@gmail.com>
2024-04-10 20:42:18 +0300

    restore to initial load_data method where one language dataset is loaded

--- src/training/load_data.py
+++ src/training/load_data.py
@@ -28,25 +28,6 @@ class Dataset:
         self.language_abbr = language_abbr
 
 
-    # def load_dataset(self) -> DatasetDict:
-    #     """
-    #     Load the streaming dataset.
-
-    #     Args:
-    #         split (Optional[str]): The dataset split to load. Defaults to 'train'.
-    #         **kwargs: Additional keyword arguments.
-
-    #     Returns:
-    #         DatasetDict: The loaded streaming dataset.
-    #     """
-    #     dataset = DatasetDict()
-    #     dataset['test'] = load_dataset(self.dataset_name, self.language_abbr, split="test", 
-    #                                         token=self.huggingface_token, streaming=True, 
-    #                                         trust_remote_code=True)
-    #     dataset['train'] = load_dataset(self.dataset_name, self.language_abbr, split="train", 
-    #                                         token=self.huggingface_token, streaming=True, 
-    #                                         trust_remote_code=True)
-        # return dataset
     def load_dataset(self) -> DatasetDict:
         """
         Load the streaming dataset.
@@ -59,41 +40,13 @@ class Dataset:
             DatasetDict: The loaded streaming dataset.
         """
         dataset = DatasetDict()
-
-        # Load English dataset
-        dataset_ti = load_dataset(self.dataset_name, "ti", 
-                                    token=self.huggingface_token, streaming=True, 
-                                    trust_remote_code=True)
-
-        # Load Swahili dataset
-        dataset_yi= load_dataset(self.dataset_name, "af", 
-                                    token=self.huggingface_token, streaming=True, 
-                                    trust_remote_code=True)
-        dataset['train'] = interleave_datasets([dataset_ti['train'], dataset_yi['train']])
-        dataset['test'] = interleave_datasets([dataset_ti['test'], dataset_yi['test']])
+        dataset['test'] = load_dataset(self.dataset_name, self.language_abbr, split="test", 
+                                            token=self.huggingface_token, streaming=True, 
+                                            trust_remote_code=True)
+        dataset['train'] = load_dataset(self.dataset_name, self.language_abbr, split="train", 
+                                            token=self.huggingface_token, streaming=True, 
+                                            trust_remote_code=True)
         return dataset
-    
-    # def load_dataset(self) -> DatasetDict:
-    #     """
-    #     Load the streaming dataset for the specified language(s).
-
-    #     Returns:
-    #         DatasetDict: The loaded streaming dataset.
-    #     """
-    #     dataset = DatasetDict()
-
-    #     for lang in self.language_abbr:
-    #         lang_dataset = load_dataset(self.dataset_name, lang,
-    #                                     token=self.huggingface_token, streaming=True,
-    #                                     trust_remote_code=True)
-    #         if 'train' not in dataset:
-    #             dataset['train'] = lang_dataset['train']
-    #             dataset['test'] = lang_dataset['test']
-    #         else:
-    #             dataset['train'] = interleave_datasets([dataset['train'], lang_dataset['train']])
-    #             dataset['test'] = interleave_datasets([dataset['test'], lang_dataset['test']])
-
-    #     return dataset
         
     def count_examples(self, dataset: IterableDataset) -> int:
         """
