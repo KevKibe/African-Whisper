4d8fbde1c55ee619dd626e6a3c3aa7bd938fb940
KevKibe <kchegz234@gmail.com>
2024-04-12 13:37:57 +0300

    add: test for training.load_data module

--- src/tests/test_dataset.py
+++ src/tests/test_dataset.py
@@ -1,84 +1,44 @@
 import unittest
-from unittest.mock import patch, MagicMock
-import sys
+from training.load_data import Dataset
 
-sys.path.append("load_data")
-from src.load_data import Dataset
-
-# from load_data import Dataset
-
-
-class TestDataset(unittest.TestCase):
+class TestDatasetManager(unittest.TestCase):
     def setUp(self):
-        self.huggingface_token = "your_token"
-        self.dataset_name = "your_dataset_name"
-        self.language_abbr = "your_language_abbr"
         self.dataset_manager = Dataset(
-            self.huggingface_token, self.dataset_name, self.language_abbr
+            huggingface_token="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",
+            dataset_name="mozilla-foundation/common_voice_16_1",
+            language_abbr=["yi","ti"]
         )
 
-    @patch("os.path.exists")
-    @patch("datasets.load_dataset")
-    def test_load_dataset_from_cache(self, mock_load_dataset, mock_os_path_exists):
-        mock_os_path_exists.return_value = True
-        # Mock the returned dataset
-        mock_dataset = MagicMock()
-        mock_load_dataset.return_value = mock_dataset
+    def test_load_dataset(self):
+        # Arrange
+        # Act
+        data = self.dataset_manager.load_dataset()
+        # Assert
+        self.assertIsNotNone(data)
+        self.assertTrue("train" in data)
+        self.assertTrue("test" in data)
 
-        dataset_dict = self.dataset_manager.load_dataset()
+    def test_count_examples(self):
+        # Arrange
+        class MockDataset:
+            def __iter__(self):
+                return iter(range(10))
+        # Act
+        count = self.dataset_manager.count_examples(MockDataset())
+        # Assert
+        self.assertEqual(count, 10)
 
-        mock_load_dataset.assert_called_with(
-            self.dataset_name,
-            self.language_abbr,
-            split="train",
-            token=self.huggingface_token,
-            streaming=False,
-            trust_remote_code=True,
-            cache_dir=f"./{self.language_abbr}",
-        )
-        mock_load_dataset.assert_called_with(
-            self.dataset_name,
-            self.language_abbr,
-            split="test",
-            token=self.huggingface_token,
-            streaming=False,
-            trust_remote_code=True,
-            cache_dir=f"./{self.language_abbr}",
-        )
-        self.assertEqual(dataset_dict["train"], mock_dataset)
-        self.assertEqual(dataset_dict["test"], mock_dataset)
+    def test_dataset_structure(self):
+        # Arrange
+        expected_features = ['client_id', 'path', 'audio', 'sentence', 'up_votes', 'down_votes', 'age', 'gender', 'accent', 'locale', 'segment', 'variant']
+        # Act
+        dataset = self.dataset_manager.load_dataset()
+        # Assert
+        train_features = list(dataset['train'].features.keys())
+        self.assertEqual(train_features, expected_features)
 
-    @patch("os.path.exists")
-    @patch("datasets.load_dataset")
-    def test_load_dataset_from_remote(self, mock_load_dataset, mock_os_path_exists):
-        mock_os_path_exists.return_value = False
-        # Mock the returned dataset
-        mock_dataset = MagicMock()
-        mock_load_dataset.return_value = mock_dataset
+        test_features = list(dataset['test'].features.keys())
+        self.assertEqual(test_features, expected_features)
 
-        dataset_dict = self.dataset_manager.load_dataset()
-
-        mock_load_dataset.assert_called_with(
-            self.dataset_name,
-            self.language_abbr,
-            split="train",
-            token=self.huggingface_token,
-            streaming=False,
-            trust_remote_code=True,
-            cache_dir=f"./{self.language_abbr}",
-        )
-        mock_load_dataset.assert_called_with(
-            self.dataset_name,
-            self.language_abbr,
-            split="test",
-            token=self.huggingface_token,
-            streaming=False,
-            trust_remote_code=True,
-            cache_dir=f"./{self.language_abbr}",
-        )
-        self.assertEqual(dataset_dict["train"], mock_dataset)
-        self.assertEqual(dataset_dict["test"], mock_dataset)
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
