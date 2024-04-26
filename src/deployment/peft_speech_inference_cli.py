287036a19b37994486cd5158c9e6ceee7a21c713
KevKibe <kchegz234@gmail.com>
2024-04-26 22:12:25 +0300

    fix: argparse name

--- src/deployment/peft_speech_inference_cli.py
+++ src/deployment/peft_speech_inference_cli.py
@@ -15,11 +15,11 @@ def main():
     speech_inference = SpeechInference(model_name=model_name, huggingface_read_token=huggingface_read_token)
     pipe = speech_inference.pipe_initialization()
     
-    if not os.path.exists(args.input_file):
-        print(f"Error: Input file '{args.input_file}' does not exist.")
+    if not os.path.exists(args.audio_file):
+        print(f"Error: Input file '{args.audio_file}' does not exist.")
         return
     
-    transcription = speech_inference.output(pipe=pipe, input=args.input_file, task=args.task)
+    transcription = speech_inference.output(pipe=pipe, input=args.audio_file, task=args.task)
     print(transcription.text)
 
 if __name__ == "__main__":
