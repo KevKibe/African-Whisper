3f59d5de694760ee45d0d0d802ded283ffe8c350
Kevin Kibe <kchegz234@gmail.com>
2024-05-21 12:41:32 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ ALL_DEPS = BASE_DEPS + DEPLOYMENT_DEPS
 
 setup(
     name="africanwhisper",
-    version="0.9.3",
+    version="0.9.4",
     author="Kevin Kibe",
     author_email="keviinkibe@gmail.com",
     package_dir={"": "src"},
