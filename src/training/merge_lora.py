23e732c08a7aa54edf8032b36bc503dac9a8de74
KevKibe <kchegz234@gmail.com>
2024-06-14 19:50:57 +0300

    feat: add Merger.merge_lora_weights method

--- src/training/merge_lora.py
+++ src/training/merge_lora.py
@@ -1,20 +1,22 @@
 from transformers import WhisperForConditionalGeneration
 from peft import PeftModel, PeftConfig
 
+class Merger:
+    def merge_lora_weights(hf_model_id, huggingface_write_token):
+        """
+        Merge LoRA weights with a pre-trained Whisper model and upload the merged model to the Hugging Face Hub.
 
-def merge_lora_weights(hf_model_id, output_dir, huggingface_write_token):
-    # Load the model configuration from Hugging Face
-    peft_config = PeftConfig.from_pretrained(hf_model_id)
+        Args:
+            hf_model_id (str): The Hugging Face model ID for the LoRA configuration.
+            huggingface_write_token (str): The Hugging Face write token for authentication.
 
-    # Load the base model and the LoRA model
-    base_model = WhisperForConditionalGeneration.from_pretrained(peft_config.base_model_name_or_path)
-    model = PeftModel.from_pretrained(base_model, hf_model_id)
-
-    # Merge the weights and unload the LoRA model
-    model = model.merge_and_unload()
-    model.train(False)
-
-    # Push the model to the Hugging Face Model Hub
-    model.push_to_hub(repo_id=output_dir, token=huggingface_write_token)
-
-    return model
+        Returns:
+            PeftModel: The merged model.
+        """
+        peft_config = PeftConfig.from_pretrained(hf_model_id)
+        base_model = WhisperForConditionalGeneration.from_pretrained(peft_config.base_model_name_or_path)
+        model = PeftModel.from_pretrained(base_model, hf_model_id)
+        model = model.merge_and_unload()
+        model.train(False)
+        model.push_to_hub(repo_id=hf_model_id, token=huggingface_write_token)
+        print(f"{hf_model_id} LoRA weights merged")
