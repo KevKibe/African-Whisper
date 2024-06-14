7bedea1341fc763189168dd45a28e73598204676
KevKibe <kchegz234@gmail.com>
2024-06-14 18:08:50 +0300

    fix:  implementation of merge lora weights

--- src/training/merge_lora.py
+++ src/training/merge_lora.py
@@ -2,10 +2,19 @@ from transformers import WhisperForConditionalGeneration
 from peft import PeftModel, PeftConfig
 
 
-def merge_lora_weights(lora_model, output_dir, huggingface_write_token):
-    peft_config = PeftConfig.from_pretrained(lora_model)
+def merge_lora_weights(hf_model_id, output_dir, huggingface_write_token):
+    # Load the model configuration from Hugging Face
+    peft_config = PeftConfig.from_pretrained(hf_model_id)
+
+    # Load the base model and the LoRA model
     base_model = WhisperForConditionalGeneration.from_pretrained(peft_config.base_model_name_or_path)
-    model = PeftModel.from_pretrained(base_model, lora_model)
+    model = PeftModel.from_pretrained(base_model, hf_model_id)
+
+    # Merge the weights and unload the LoRA model
     model = model.merge_and_unload()
     model.train(False)
-    model.push_to_hub(repo_id = output_dir, token = huggingface_write_token)
+
+    # Push the model to the Hugging Face Model Hub
+    model.push_to_hub(repo_id=output_dir, token=huggingface_write_token)
+
+    return model
