31ce34f872930892eb00c16368ed914115a11bc8
Kevin Kibe <kchegz234@gmail.com>
2024-07-11 18:44:01 +0300

    Update package version to 0.9.10

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ ALL_DEPS = BASE_DEPS + DEPLOYMENT_DEPS
 
 setup(
     name="africanwhisper",
-    version="0.9.9",
+    version="0.9.10",
     author="Kevin Kibe",
     author_email="keviinkibe@gmail.com",
     package_dir={"": "src"},
